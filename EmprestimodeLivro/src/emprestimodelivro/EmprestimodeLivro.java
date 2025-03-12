76a301aeef7245bd727dbcce661a0a02fb613001
gabymello <mellogaby689@gmail.com>
2025-03-12 14:39:53 -0300

    Add files via upload

---
+++ EmprestimodeLivro/src/emprestimodelivro/EmprestimodeLivro.java
@@ -0,0 +1,20 @@
+
+package emprestimodelivro;
+
+
+public class EmprestimodeLivro {
+
+   
+    public static void main(String[] args) {
+       Livro livro = new Livro("O Hobbit", "J.R.R. Tolkien");
+        System.out.println("Livro: " + livro.getTitulo() + ", Disponível: " + livro.isDisponivel());
+        livro.emprestar();
+        System.out.println("Após empréstimo: " + livro.isDisponivel());
+        livro.devolver();
+        System.out.println("Após devolução: " + livro.isDisponivel());
+    }
+}
+
+    
+    
+
